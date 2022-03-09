9bc76bbfaa86140b90d698942309897cfd571512
sarah-hamilton321 <hamilton.sarah321@gmail.com>
2022-03-09 10:40:43 -0800

    updated example package

---
+++ R/fish.R
@@ -0,0 +1,10 @@
+#' Random fish catch data
+#'
+#' Data of random types of fish (Description)
+#'
+#'@format A list of of 4 fish species
+#'\itemize{
+#'\item species species of fish caught
+#'}
+#'@source made up
+"fish"
